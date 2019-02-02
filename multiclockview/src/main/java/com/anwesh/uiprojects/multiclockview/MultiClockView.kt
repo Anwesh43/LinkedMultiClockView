d1f60b798acfe59ae5465d5986588dcae69022db
Anwesh43 <anweshthecool0@gmail.com>
2019-02-02 11:44:58 +0530

    adding Animator class

--- multiclockview/src/main/java/com/anwesh/uiprojects/multiclockview/MultiClockView.kt
+++ multiclockview/src/main/java/com/anwesh/uiprojects/multiclockview/MultiClockView.kt
@@ -64,4 +64,32 @@ class MultiClockView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
