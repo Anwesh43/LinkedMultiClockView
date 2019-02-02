fdad2f730954925fc6ed68db590ccb7d242317e4
Anwesh43 <anweshthecool0@gmail.com>
2019-02-02 11:35:58 +0530

    adding libraries and constants

--- multiclockview/src/main/java/com/anwesh/uiprojects/multiclockview/MultiClockView.kt
+++ multiclockview/src/main/java/com/anwesh/uiprojects/multiclockview/MultiClockView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.multiclockview
 /**
  * Created by anweshmishra on 02/02/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val nodes : Int = 5
+val clocks : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#512DA8")
+val backColor : Int = Color.parseColor("#BDBDBD")
